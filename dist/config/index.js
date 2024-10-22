c473dfc284527d51fc221d40d8bc89101a9fb203
adislksn <adi.120140038@student.itera.ac.id>
2024-10-23 02:02:01 +0700

    feat: vercel delet

--- dist/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONFIG = void 0;
-exports.CONFIG = {
-    port: 3000,
-    urlVersioning: '/api/v1',
-    urlBase: 'http://localhost:3000',
-};
-//# sourceMappingURL=index.js.map
