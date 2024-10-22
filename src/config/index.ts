8dcf0885f1214ff7c51d7bb1d45bb56432c4383e
adislksn <adi.120140038@student.itera.ac.id>
2024-10-23 04:19:08 +0700

    feat: docker setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,15 @@
-export const CONFIG = {
-    port: 3000,
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+type ConfigEnv = {
+    port: number,
+    urlVersioning: string,
+    urlBase: string,
+}
+
+export const CONFIG:ConfigEnv = {
+    port: Number(process.env.APP_PORT ?? 3000),
     urlVersioning: '/api/v1',
-    urlBase: 'http://localhost:3000',
+    urlBase: process.env.APP_BASE_URL ?? 'http://localhost:3000'
 }
